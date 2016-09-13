919bc7e9ba4996981baf094e2fe7d28f6b623745
LINEBARRELS <954526862@qq.com>
2016-09-13 10:27:37 +0800

    slide

--- main.js
+++ main.js
@@ -2,34 +2,51 @@ paper = {
     pages:[],
     border:[],
     timer:null,
+    cur:0,
+    height:null,
     init:function(){
-
-    document.body.onmousewheel = function(e){
-    e.preventDefault();
-    clearTimeout(this.timer);
-    this.timer=setTimeout(function(){
-    switch(e.wheelDelta){
-    case 120:
-             $('body').animate({scrollTop: document.body.scrollTop - 685}, 500)
-             console.log('上');
-            break;
-    case -120:$('body').animate({scrollTop: document.body.scrollTop + 685}, 500)
-             console.log('下');
-            break;
-       }
-    }, 500);
-    }
  
-    $('.part').each(function(index, el) {
-    	this.pages.push(el)
-    }.bind(this));
-    
-    for (let i = 1; i < this.pages.length; i++) {
-    	this.border.push(this.pages[i].offsetTop)
-    };
+     this.height = document.querySelector('.part').offsetHeight;
+     document.querySelector('.main').onmousewheel = function(e){
+     e.preventDefault();
+     clearTimeout(this.timer);
+     this.timer=setTimeout(function(){
+     switch(e.wheelDelta){
+     case 120:
+             if(this.cur!=0){
+                this.cur -= 1;
+             }
+             $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
+             console.log('上');
+             if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
+            this.pages[this.cur].classList.remove('hid')
+        }
+            break;
+     case -120:
+              this.cur += 1;
+              console.log(this.cur);
+              $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
+              console.log('下');
+              if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
+            this.pages[this.cur].classList.remove('hid')
+             }
+             break;
+        }
+        
+      }.bind(this), 500);
 
-    setTimeout(function(){this.pages[0].classList.remove('hid');}.bind(this),500);
-    
+     }.bind(this)
+  
+     $('.part').each(function(index, el) {
+     	this.pages.push(el)
+     }.bind(this));
+     
+     for (let i = 1; i < this.pages.length; i++) {
+     	this.border.push(this.pages[i].offsetTop)
+     };
+ 
+     setTimeout(function(){this.pages[0].classList.remove('hid');}.bind(this),500);
+     
     }//init
 }
 
