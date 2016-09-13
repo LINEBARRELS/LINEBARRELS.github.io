958f801a55951e3a6572de04ad8ac094701ad221
LINEBARRELS <954526862@qq.com>
2016-09-13 18:32:23 +0800

    fir

--- main.js
+++ main.js
@@ -4,33 +4,56 @@ paper = {
     timer:null,
     cur:0,
     height:null,
+    inited:null,
+    dealing:function(side,option){
+     switch(side){
+     case 1:{ 
+             console.log(this.cur);
+             if(this.cur!=0){
+                this.cur -= option;
+             }
+             $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
+             console.log('上');
+            if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
+            this.pages[this.cur].classList.remove('hid')
+            }
+            if(this.inited[this.cur]==='none'&&this.pages[this.cur].init){
+                this.pages[this.cur].init();
+            }
+            break;
+            }
+     case 2:{
+            console.log(this.cur);
+              if(this.cur!=this.pages.length-1){
+                this.cur += option;
+              }
+              
+            
+              $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
+              console.log('下');
+              if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
+              this.pages[this.cur].classList.remove('hid')
+             }
+             if(this.inited[this.cur]==='none'&&this.pages[this.cur].init){
+                this.pages[this.cur].init();
+            }
+             break;
+            }
+        }
+
+    },
     init:function(){
  
-     this.height = document.querySelector('.part').offsetHeight;
+     this.height = document.querySelector('.part').offsetHeight ;
      document.querySelector('.main').onmousewheel = function(e){
      e.preventDefault();
      clearTimeout(this.timer);
      this.timer=setTimeout(function(){
-     switch(e.wheelDelta){
-     case 120:
-             if(this.cur!=0){
-                this.cur -= 1;
-             }
-             $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
-             console.log('上');
-             if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
-            this.pages[this.cur].classList.remove('hid')
-        }
-            break;
-     case -120:
-              this.cur += 1;
-              console.log(this.cur);
-              $('.main').css('transform','translateY(-'+this.cur*this.height+'px)');
-              console.log('下');
-              if (Array.prototype.slice.call(this.pages[this.cur].classList).indexOf('hid')!=-1) {
-            this.pages[this.cur].classList.remove('hid')
-             }
-             break;
+        switch(e.wheelDelta){
+            case 120:this.dealing(1,1);
+                     break;
+            case -120:this.dealing(2,1);
+                    break;
         }
         
       }.bind(this), 500);
@@ -44,8 +67,30 @@ paper = {
      for (let i = 1; i < this.pages.length; i++) {
      	this.border.push(this.pages[i].offsetTop)
      };
+
+    paper.pages[0].init = function(){
+    console.log('第一页初始化');
+    paper.inited[0]='complete';
+    }
+
+    paper.pages[1].init = function(){
+        console.log('第二页初始化');
+        paper.inited[1]='complete';
+    }
+    
+    paper.pages[2].init = function(){
+        console.log('第三页初始化');
+             paper.inited[2]='complete';
+    }
+
+     this.inited = Array(this.pages.length).fill('none');
+
+     
+
+     $('.part').css('height', 'value');
  
      setTimeout(function(){this.pages[0].classList.remove('hid');}.bind(this),500);
+     this.pages[0].init()
      
     }//init
 }
